8822d1323e3d34ccd1d702bb9ba5513b72ad7a90
Przmek Wójcik <przemek.wojcik@gmail.com>
2018-10-09 21:51:36 +0200

    AddingExercise2

---
+++ src/Exercise2/ConsumerImplementation.java
@@ -0,0 +1,22 @@
+package Exercise2;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.function.Consumer;
+
+public class ConsumerImplementation {
+    public static void main(String[] args) {
+
+        final DateTimeFormatter time = DateTimeFormatter.ofPattern("HH:mm");
+        LocalTime now = LocalTime.now();
+
+        Consumer<String> substringTime = (newNow) -> {
+            System.out.println("Godzina: " + newNow.substring(0,2));
+            System.out.println("Minuty: " + newNow.substring(3,5));
+        };
+        showTime(substringTime, time.format(now));
+    }
+    static void showTime (Consumer<String> substringTime, String time) {
+        substringTime.accept(time);
+    }
+}
