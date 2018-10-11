74780b0d0108673f99feb5ac705a15bfedc79e2f
Przmek Wójcik <przemek.wojcik@gmail.com>
2018-10-11 22:20:22 +0200

    OptimazingExercise1

--- src/Exersise1/UserDemo.java
+++ src/Exersise1/UserDemo.java
@@ -2,13 +2,6 @@ package Exersise1;
 
 public class UserDemo {
     public static void main(String[] args) {
-        User user1 = new User("Jan", "Kowalski");
-        User user2 = new User("Adam", "Nowak");
-        User user3 = new User("Jan", "Pawlak");
-
-        UserService.users.add(user1);
-        UserService.users.add(user2);
-        UserService.users.add(user3);
 
         System.out.println(UserService.findByName("Karol"));
         System.out.println(UserService.findByName("Jan"));
--- src/Exersise1/UserService.java
+++ src/Exersise1/UserService.java
@@ -1,12 +1,18 @@
 package Exersise1;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 public class UserService {
 
-    protected final static List<User> users = new ArrayList<>();
+    protected final static List<User> users = Arrays.asList(
+            new User("Jan", "Kowalski"),
+            new User ("Adam", "Nowak"),
+            new User("Jan", "Pawlak")
+    );
+
     protected final static List<User> searchedUsers = new ArrayList<>();
 
     protected static Optional<User> findByName(String usersName) {
