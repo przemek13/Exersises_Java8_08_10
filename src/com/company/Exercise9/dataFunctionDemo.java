a760a11e33957d4f30fda8d0b0a85772d2db486f
Przmek Wójcik <przemek.wojcik@gmail.com>
2018-10-12 13:05:08 +0200

    OptimizingExercise3

--- src/com/company/Exercise9/dataFunctionDemo.java
+++ src/com/company/Exercise9/dataFunctionDemo.java
@@ -1,7 +1,5 @@
 package com.company.Exercise9;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
