bd243f1522583a10f70a6623446add4958e0239d
Przmek Wójcik <przemek.wojcik@gmail.com>
2018-10-15 13:44:49 +0200

    Ex1.AfterCodeReviewFix

--- src/Exersise1/UserDemo.java
+++ src/Exersise1/UserDemo.java
@@ -3,10 +3,11 @@ package Exersise1;
 public class UserDemo {
     public static void main(String[] args) {
 
+        System.out.println(UserService.findByNameList("Karol"));
+        System.out.println(UserService.findByNameList("Jan"));
+
         System.out.println(UserService.findByName("Karol"));
         System.out.println(UserService.findByName("Jan"));
 
-        System.out.println(UserService.findByNameList("Karol"));
-        System.out.println(UserService.findByNameList("Jan"));
     }
 }
--- src/Exersise1/UserService.java
+++ src/Exersise1/UserService.java
@@ -7,30 +7,31 @@ import java.util.Optional;
 
 public class UserService {
 
-    protected final static List<User> users = Arrays.asList(
+    protected final static List<User> USERS = Arrays.asList(
             new User("Jan", "Kowalski"),
-            new User ("Adam", "Nowak"),
+            new User("Adam", "Nowak"),
             new User("Jan", "Pawlak")
     );
 
-    protected final static List<User> searchedUsers = new ArrayList<>();
+    protected final static List<User> SEARCHED_USERS = new ArrayList<>();
 
     protected static Optional<User> findByName(String usersName) {
-        User searchedUser = null;
-        for (User user : users) {
-            if (usersName.equals(user.getFirstName())) {
-                searchedUser = user;
-            }
-        }
-        return Optional.ofNullable(searchedUser);
+        return Optional.ofNullable(searchList(usersName));
     }
 
     protected static Optional<List<User>> findByNameList(String usersName) {
-        for (User user : users) {
-            if (usersName.equals(user.getFirstName())) {
-                searchedUsers.add(user);
+        searchList(usersName);
+        return Optional.ofNullable(SEARCHED_USERS);
+    }
+
+    private static User searchList(String userName) {
+        User searchedUser = null;
+        for (User user : USERS) {
+            if (userName.equals(user.getFirstName())) {
+                searchedUser = user;
+                SEARCHED_USERS.add(user);
             }
         }
-        return Optional.ofNullable(searchedUsers);
+        return searchedUser;
     }
 }
