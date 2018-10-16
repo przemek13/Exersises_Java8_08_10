60099ab68d4daadfaeefba6b96d0c82af25e5278
Przmek Wójcik <przemek.wojcik@gmail.com>
2018-10-16 09:49:35 +0200

    Ex4.AfterCodeReviewFix

--- src/com/company/Exercise4/UserDemo.java
+++ src/com/company/Exercise4/UserDemo.java
@@ -7,12 +7,14 @@ public class UserDemo {
 
     public static void main(String[] args) {
 
+        int ageOfMajority = 18;
+
         Predicate<User> ageCheck = (user) -> {
             System.out.print(user.getUserName() + " is of age: ");
-            if (user.returnUserAge(user) > 18) {
+            if (user.returnUserAge(user) > ageOfMajority) {
                 return true;
             } else return false;
         };
-        System.out.print(ageCheck.test(new User("Adam Nowak", LocalDate.of(1999, 10, 10))));
+        System.out.print(ageCheck.test(new User("Adam Nowak", LocalDate.of(1999, 10, 17))));
     }
 }
